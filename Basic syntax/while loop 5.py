a5bdc40ebaf8407e31dfa5bc2e86b6ecc2d806d5
trinh khanh <trinhkhanh0609@gmail.com>
2015-08-31 21:06:10 +0700

    upload

---
+++ Basic syntax/while loop 5.py
@@ -0,0 +1,13 @@
+__author__ = 'trinhkhanh'
+while True:
+    n1 = int(input('Enter first integer: '))
+    n2 = int(input('Enter second integer: '))
+
+    gcd = 1
+    k = 2
+    while k <= n1 and k<= n2:
+        if n1%k == 0 and n2%k == 0:
+            gcd = k
+        k += 1
+
+    print('The greatest common divisor: %d'%gcd)
