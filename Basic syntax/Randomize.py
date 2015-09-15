be2e07bf64376dfbd7c45406d7681a04cf8fdede
trinh khanh <trinhkhanh0609@gmail.com>
2015-09-15 22:11:22 +0700

    amend

--- Basic syntax/Randomize.py
+++ Basic syntax/Randomize.py
@@ -7,9 +7,9 @@ squad = [['Marc-Andre ter Stegen', 'Claudio Bravo', 'Jordi Masip'],
          ['Ivan Rakitic', 'Sergio Busquets', 'Andres Iniesta', 'Neymar', 'Rafinha', 'Javier Mascherano', 'Sergi Roberto', 'Denis Suarez'],
          ['Luis Suarez', 'Lionel Messi', 'Sandro', 'Munir', 'Cristian Tello']]
 
-TM = random.sample(squad[0], 1)# TM: chon mot thu mon
+TM = random.sample(squad[0], 1) # TM: Chon mot thu mon
 HV = random.sample(squad[1], 4) # HV: Hau ve chon 2 tru - 2 bien
-TV = random.sample(squad[2], 4) # TV: chon 4 tien ve
-TD = random.sample(squad[3], 2) #TD: chon 2 tien dao
+TV = random.sample(squad[2], 4) # TV: Chon 4 tien ve
+TD = random.sample(squad[3], 2) # TD: Chon 2 tien dao
 
 print('Doi hinh ra san gom: %s, %s, %s, %s'%(TM, HV, TV, TD))
