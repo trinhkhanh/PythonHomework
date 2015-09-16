1120150fc1b23c93814569569bb94e78db6ebd3d
trinh khanh <trinhkhanh0609@gmail.com>
2015-09-16 21:38:24 +0700

    upload

---
+++ DEMO FOR ALL/Zip function.py
@@ -0,0 +1,12 @@
+__author__ = 'trinhkhanh'
+x = [9, 2, 3, 2, 1, 2]
+y = [4, 5, 6, 7, 12, 15]
+zipped = list()
+
+for x, y in zip(x, y):
+    if (x+y) <= 10:
+        zipped.append([x,y])
+print (zipped)
+
+
+
