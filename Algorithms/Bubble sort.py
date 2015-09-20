2d6a579666c25f3abad8f03806ae943595a89fec
trinh khanh <trinhkhanh0609@gmail.com>
2015-09-20 11:50:05 +0700

    amend for first upload

--- Algorithms/Bubble sort.py
+++ Algorithms/Bubble sort.py
@@ -1,30 +1,19 @@
 __author__ = 'trinhkhanh'
-import random
-characterList = []
-# Goi ra list tu a-z
-for x in range(ord('a'), ord('z')+1):
-    characterList.append(x)
-# Lay ra 5 - 10 ky tu trong list va sap xep theo thu tu
-randomList = random.sample(characterList, random.randint(5, 10))
-for i in range(0, len(randomList) - 1):
-    for j in range(i + 1, len(randomList)):
+import string, random
+
+randomList = []
+for number in range (5):
+    randomList.append(''.join(random.sample(string.ascii_lowercase,random.randint(5,10))))
+print(randomList)
+
+for i in range(0, len(randomList)- 1):
+    for j in range(i+1,len(randomList)):
         if randomList[i] > randomList[j]:
             temp = randomList[i]
             randomList[i] = randomList[j]
             randomList[j] = temp
-# In ra man hinh ket qua
-resultList = []
-for randomList in reversed(randomList):
-    randomList = (str(chr(randomList))).upper()
-    resultList.append(randomList)
-print(resultList)
 
-
-
-
-
-
-
+print(randomList)
 
 
 
