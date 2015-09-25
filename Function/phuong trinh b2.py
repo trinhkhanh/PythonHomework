64a0d0c9776ec7bdc5e5b0895d6803874c8d7f59
trinh khanh <trinhkhanh0609@gmail.com>
2015-09-25 22:28:02 +0700

    amend for first upload

---
+++ Function/phuong trinh b2.py
@@ -0,0 +1,25 @@
+__author__ = 'trinhkhanh'
+
+import math
+
+def heSo(a, b, c):
+    if a!= 0:
+        delta = b**2 - 4*a*c
+        if delta > 0:
+            return (-b - math.sqrt(delta))/(2*a), (-b + math.sqrt(delta))/(2*a)
+        elif delta < 0:
+            return ('Phuong trinh vo nghiem')
+        else:
+            return -b/(2*a)
+    else:
+        if b != 0:
+            return -c/b
+        else:
+            return None
+
+
+print(heSo(1, -6, 9))
+
+
+
+
