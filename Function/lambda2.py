b0454db59b3bceb8fde6699ff355c382b66446bc
trinh khanh <trinhkhanh0609@gmail.com>
2015-09-28 22:32:12 +0700

    amend

--- Function/lambda2.py
+++ Function/lambda2.py
@@ -1,4 +1,5 @@
 __author__ = 'trinhkhanh'
+
 import functools
 #  Using lambda function to calculate the sum of odd number in the foo
 foo = [2, 18, 9, 22, 17, 24, 8, 12, 27]
@@ -8,10 +9,7 @@ print()
 #  Using def function to calculate the sum of odd number in the foo
 def add(x, y):
     return x+y
-result =[]
-for i in foo:
-    if i % 2 ==1:
-        result.append(i)
+result =[i for i in foo if i%2 == 1]
 print(functools.reduce(add, result))
 
 
