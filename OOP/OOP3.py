04ab933fa8b8164aede637d852dc4f97887337f1
trinh khanh <trinhkhanh0609@gmail.com>
2015-10-11 10:19:29 +0700

    amend for first upload

---
+++ OOP/OOP3.py
@@ -0,0 +1,11 @@
+__author__ = 'trinhkhanh'
+class useBMIClass:
+
+    def __int__(self, name, age, weight, height):
+        self.name = name
+        self.age = age
+        self.weight = weight
+        self.height = height
+        BMI = weight/height**2
+    def checkBMI:
+        if
