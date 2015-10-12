f1be4df9c7e34e1fdbda84ab8961a76136514a2f
trinh khanh <trinhkhanh0609@gmail.com>
2015-10-12 23:43:09 +0700

    amend

--- OOP/OOP3.py
+++ OOP/OOP3.py
@@ -1,27 +1,29 @@
 __author__ = 'trinhkhanh'
 class UseBMIClass:
 
-    def __int__(self, name, age, weight, height):
+    def __init__(self, name, age, weight, height):
         self.name = name
         self.age = age
         self.weight = weight
         self.height = height
-    def bMI(self):
-        self.BMI = self.weight/self.height**2
+    def BMI(self):
+        return (self.weight * 0.45359237)/(self.height*0.0254)**2
 
     def checkBMI(self):
-        if self.BMI >= 18.5:
-            if self.BMI < 25:
-                print('The BMI for ', str(self.name),'is', self.BMI, 'normal')
+        if self.BMI() >= 18.5:
+            if self.BMI() < 25:
+                print('The BMI for {0} is {1:.2f} Normal'.format(self.name,self.BMI()))
             else:
-                if self.BMI == 25 and self.BMI < 30:
-                    print('The BMI for ', str(self.name),'is', self.BMI, 'overweight')
+                if self.BMI() == 25 and self.BMI() < 30:
+                    print('The BMI for {0} is {1:.2f} overweight'.format(self.name,self.BMI()))
                 else:
-                    print('The BMI for ', str(self.name),'is', self.BMI, 'obese')
+                    print('The BMI for {0} is {1:.2f} obese'.format(self.name,self.BMI()))
         else:
-            print('The BMI for ', str(self.name),'is', self.BMI, 'underweight')
+            print('The BMI for {0} is {1:.2f} underweight'.format(self.name,self.BMI()))
 
 
-new1BMI = UseBMIClass('Kim Yang', 18, 145, 70)
+newBMI1 = UseBMIClass('Kim Yang', 18, 145, 70)
+newBMI2 = UseBMIClass('Susan King', 18, 215, 70)
 
-new1BMI.checkBMI()
+newBMI1.checkBMI()
+newBMI2.checkBMI()
