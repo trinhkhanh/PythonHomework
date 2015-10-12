2181bb6e7f120e1254faeb0d58ae5b737ae8f79b
trinh khanh <trinhkhanh0609@gmail.com>
2015-10-12 21:44:12 +0700

    upload

--- OOP/OOP3.py
+++ OOP/OOP3.py
@@ -1,11 +1,27 @@
 __author__ = 'trinhkhanh'
-class useBMIClass:
+class UseBMIClass:
 
     def __int__(self, name, age, weight, height):
         self.name = name
         self.age = age
         self.weight = weight
         self.height = height
-        BMI = weight/height**2
-    def checkBMI:
-        if
+    def bMI(self):
+        self.BMI = self.weight/self.height**2
+
+    def checkBMI(self):
+        if self.BMI >= 18.5:
+            if self.BMI < 25:
+                print('The BMI for ', str(self.name),'is', self.BMI, 'normal')
+            else:
+                if self.BMI == 25 and self.BMI < 30:
+                    print('The BMI for ', str(self.name),'is', self.BMI, 'overweight')
+                else:
+                    print('The BMI for ', str(self.name),'is', self.BMI, 'obese')
+        else:
+            print('The BMI for ', str(self.name),'is', self.BMI, 'underweight')
+
+
+new1BMI = UseBMIClass('Kim Yang', 18, 145, 70)
+
+new1BMI.checkBMI()
