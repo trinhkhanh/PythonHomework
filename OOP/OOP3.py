10a91779864be8e9ad509f24ddf45de6e9d0ebef
trinh khanh <trinhkhanh0609@gmail.com>
2015-10-15 23:03:31 +0700

    3rd amendment

--- OOP/OOP3.py
+++ OOP/OOP3.py
@@ -2,34 +2,39 @@ __author__ = 'trinhkhanh'
 class BMI:
 
     def __init__(self, name, age, weight, height):
-        self.name = name
-        self.age = age
+        self.name = str(name)
+        self.age = int(age)
         self.weight = weight
         self.height = height
     def getBMI(self):
         return (self.weight * 0.45359237)/(self.height*0.0254)**2
 
-    def getstatus(self):
+    def getStatus(self):
         if self.getBMI() >= 18.5:
             if self.getBMI() < 25:
-                print('{:.2f} Normal'.format(self.getBMI()))
+                return ('{:.2f} Normal'.format(self.getBMI()))
             else:
                 if self.getBMI() == 25 and self.getBMI() < 30:
-                    print('{:.2f} overweight'.format(self.getBMI()))
+                    return ('{:.2f} Overweight'.format(self.getBMI()))
                 else:
-                    print('{:.2f} obese'.format(self.getBMI()))
+                    return ('{:.2f} Obese'.format(self.getBMI()))
         else:
-            print('{:.2f} underweight'.format(self.getBMI()))
+            print('{:.2f} Underweight'.format(self.getBMI()))
 
 class UseBMIClass(BMI):
-    def display(self):
-        print('The BMI for ', self.name, 'is', self.getBMI(), ' ', self.getstatus())
+
+    def __init__(self, name, age, weight, height):
+        BMI.__init__(self, name, age, weight, height)
+
+    def getUseBMIClass(self):
+        print ('The BMI for' , self.name, 'is', self.getStatus())
 
 
-bmi1 = ('Kim Yang', 18, 145, 70)
-bmi2 = ('Susan King', 18, 215, 70)
+bmi1 = UseBMIClass('Kim Yang', 18, 145, 70)
+bmi2 = UseBMIClass('Susan King', 20, 215, 70)
 
-bmi1.display()
+bmi1.getUseBMIClass()
+bmi2.getUseBMIClass()
 
 
 
